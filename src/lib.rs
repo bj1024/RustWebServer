fbc97a28818f095963b3683a1ca05a29eb45f898
H.K <16552133+bj1024@users.noreply.github.com>
2022-06-19 17:03:58 +0900

    code format.

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,14 @@ pub struct ThreadPool {
     workers: Vec<Worker>,
 
     // Jobと、Terminate Messageを送るChannel(sender)
-    sender: mpsc::Sender<Message>,  
+    sender: mpsc::Sender<Message>,
 }
-
-
 
 // このJobはthread::spawnに渡される。
 // thread::spawn に渡す型から、 FnOnce() + Send + 'static を指定する。typeでJobという型名に。
-// シングルスレッドサーバをマルチスレッド化する - The Rust Programming Language 日本語版 
+// シングルスレッドサーバをマルチスレッド化する - The Rust Programming Language 日本語版
 // https://doc.rust-jp.rs/book-ja/ch20-02-multithreaded.html?highlight=FnOnce#%E3%82%B9%E3%83%AC%E3%83%83%E3%83%89%E3%83%97%E3%83%BC%E3%83%AB%E3%81%A7%E3%82%B9%E3%83%AB%E3%83%BC%E3%83%97%E3%83%83%E3%83%88%E3%82%92%E5%90%91%E4%B8%8A%E3%81%95%E3%81%9B%E3%82%8B
-// 
+//
 // type Jobは type alias. Jobという短縮名で定義する。
 // 高度な型 - The Rust Programming Language 日本語版
 // https://doc.rust-jp.rs/book-ja/ch19-04-advanced-types.html?highlight=type%20alias#%E5%9E%8B%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%82%B9%E3%81%A7%E5%9E%8B%E5%90%8C%E7%BE%A9%E8%AA%9E%E3%82%92%E7%94%9F%E6%88%90%E3%81%99%E3%82%8B
@@ -31,17 +29,15 @@ pub struct ThreadPool {
 // FnOnce in std::ops - Rust https://doc.rust-lang.org/std/ops/trait.FnOnce.html
 // Rustのクロージャtraitについて調べた(FnOnce, FnMut, Fn) - Qiita https://qiita.com/shortheron/items/c1735dc4c7c78b0b55e9
 
-
 // Sendでスレッド間の所有権の転送を許可する
 // SyncとSendトレイトで拡張可能な並行性 - The Rust Programming Language 日本語版 https://doc.rust-jp.rs/book-ja/ch16-04-extensible-concurrency-sync-and-send.html
 
-// Sync allows an object to to be used by two threads A and B at the same time. 
+// Sync allows an object to to be used by two threads A and B at the same time.
 // Send allows an object to be used by two threads A and B at different times.
 
 //  'staticは全プログラム期間で生存するLifeTime（ずっと参照可能なstatic。）
-// ライフタイムで参照を検証する - The Rust Programming Language 日本語版 
+// ライフタイムで参照を検証する - The Rust Programming Language 日本語版
 // https://doc.rust-jp.rs/book-ja/ch10-03-lifetime-syntax.html?highlight=%27static#%E9%9D%99%E7%9A%84%E3%83%A9%E3%82%A4%E3%83%95%E3%82%BF%E3%82%A4%E3%83%A0
-
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
@@ -56,22 +52,22 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let (sender, receiver) = mpsc::channel();   // Threadとメッセージをやりとりするチャネル。(tx,rx)が返る。
+        let (sender, receiver) = mpsc::channel(); // Threadとメッセージをやりとりするチャネル。(tx,rx)が返る。
 
         // std::sync::Arc; スマートポインタ。参照カウント方式、スレッドセーフ。(同様のstd::rc::Rc はスレッドセーフではない。)
-        // 
-        // Arc::new(Mutex::new(receiver)) 
+        //
+        // Arc::new(Mutex::new(receiver))
         // Arc<Mutex<T>>という形はデザインパターン - Rustコトハジメ https://rustforbeginners.hatenablog.com/entry/arc-mutex-design-pattern
         // 変数を複数のスレッドで共有する場合、
         //   参照を渡す(move)１つ目のmoveであとは無効でNG
         //   Arc::newし、cloneを渡す。参照のみOK
         //   Arc::new(Mutex::new(x)) のCloneを渡す。lockして変更可。
-        let receiver = Arc::new(Mutex::new(receiver)); 
+        let receiver = Arc::new(Mutex::new(receiver));
 
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));   
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool { workers, sender }
@@ -81,7 +77,7 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);  // スマートポインタ。ヒープに割り当てられる。スコープを抜けると自動で解放される。
+        let job = Box::new(f); // スマートポインタ。ヒープに割り当てられる。スコープを抜けると自動で解放される。
 
         self.sender.send(Message::NewJob(job)).unwrap(); //  チャネル経由で、関数ポインタ(JOB)を送信する。
     }
@@ -102,7 +98,7 @@ impl Drop for ThreadPool {
             println!("Shutting down worker {}", worker.id);
 
             // threadはOption。takeは変数にNoneを残しSome,Noneを取り出す。Takes the value out of the option, leaving a None in its place. https://doc.rust-lang.org/core/option/enum.Option.html#method.take
-            if let Some(thread) = worker.thread.take() {    
+            if let Some(thread) = worker.thread.take() {
                 // Waits for the associated thread to finish.
                 // This function will return immediately if the associated thread has already finished.
                 thread.join().unwrap();
@@ -116,35 +112,41 @@ struct Worker {
     thread: Option<thread::JoinHandle<()>>,
 }
 
-
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
-        let builder = thread::Builder::new()
-            .name(format!("worker{}",id).into());
+        let builder = thread::Builder::new().name(format!("worker{}", id).into());
 
-        let thread = builder.spawn(move || loop {       // loopで無限処理している。
-            let message = receiver.lock().expect("🟥🟥🟥　lock erorr.").recv().expect("🟥🟥🟥　recv erorr."); // チャネルのレシーバーをlockし、recvする。
+        let thread = builder
+            .spawn(move || loop {
+                // loopで無限処理している。
+                let message = receiver
+                    .lock()
+                    .expect("🟥🟥🟥　lock erorr.")
+                    .recv()
+                    .expect("🟥🟥🟥　recv erorr."); // チャネルのレシーバーをlockし、recvする。
 
-            match message {
-                Message::NewJob(job) => {
-                    // debug!("Worker {} got a job; executing.", id);
+                match message {
+                    Message::NewJob(job) => {
+                        // debug!("Worker {} got a job; executing.", id);
 
-                    job();
+                        job();
+                    }
+                    Message::Terminate => {
+                        info!("Worker {} was told to terminate.", id);
+
+                        break;
+                    }
                 }
-                Message::Terminate => {
-                    info!("Worker {} was told to terminate.", id);
 
-                    break;
-                }
-            }
+                // debug!("Worker {} got a job; executing.", id);
 
-            // debug!("Worker {} got a job; executing.", id);
+                // job();  // 関数ポインタ実行
+            })
+            .unwrap();
 
-            // job();  // 関数ポインタ実行
-        }).unwrap();
- 
-
-        Worker { id, 
-            thread:Some(thread) }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,38 @@
 extern crate log;
 use env_logger::Env;
 
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
+use std::str;
 use std::thread;
 use std::time::Duration;
-use std::str;
 
-use rust_web_server::ThreadPool;
 use lazy_static::lazy_static;
+use rust_web_server::ThreadPool;
 use std::sync::RwLock;
 
-
-
 lazy_static! {
-    static ref  CLIENT_NUM:RwLock<u32> = RwLock::new(0);
+    static ref CLIENT_NUM: RwLock<u32> = RwLock::new(0);
 }
 
-
-fn main() {    
+fn main() {
     let env = Env::default()
         .filter_or("MY_LOG_LEVEL", "debug")
         .write_style_or("MY_LOG_STYLE", "always");
 
     env_logger::Builder::from_env(env)
-    .format_module_path(false)
-    .format_level(true)
-    .format_timestamp_millis()
-    .format_target(false)
-    .format_indent(Some(20))
-    .init();
+        .format_module_path(false)
+        .format_level(true)
+        .format_timestamp_millis()
+        .format_target(false)
+        .format_indent(Some(20))
+        .init();
 
-    let addr ="127.0.0.1:7878";
+    let addr = "127.0.0.1:7878";
 
-    debug!("start TcpListener. {}",addr);
+    debug!("start TcpListener. {}", addr);
 
     let listener = TcpListener::bind(addr).unwrap();
     let pool = ThreadPool::new(4);
@@ -45,45 +42,58 @@ fn main() {
     // Thread終了・解放を試すために、最初の２個だけ処理する。
     // take はIterator trait に定義される。最初のN個だけ処理する。
     // for stream in listener.incoming().take(2){
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let mut clinet_num_ptr = CLIENT_NUM.write().unwrap();
-        let num  = *clinet_num_ptr; 
+        let num = *clinet_num_ptr;
 
         // debug!("CLIENT_NUM=[{}]",num);
         // let mut client_num_write = CLIENT_NUM.write().unwrap();
         *clinet_num_ptr = *clinet_num_ptr + 1;
         // *CLIENT_NUM +=1 ;
 
-            match stream {
+        match stream {
             Ok(stream) => {
-                debug!("incoming stream [{}] {:?}",num,stream.peer_addr().unwrap());
+                debug!(
+                    "incoming stream [{}] {:?}",
+                    num,
+                    stream.peer_addr().unwrap()
+                );
                 pool.execute(move || {
-                    handle_connection(num,stream);
+                    handle_connection(num, stream);
                 });
             }
-            Err(e) => {error!("connection failed {:?}",e);}
+            Err(e) => {
+                error!("connection failed {:?}", e);
+            }
         }
     }
 }
-fn handle_connection(no:u32 ,mut stream: TcpStream) {
+fn handle_connection(no: u32, mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/";
 
-   
     let sleep = b"GET /sleep HTTP/";
-    let threadsig = format!("[{}][{}]",no,thread::current().name().unwrap_or("unknown thread"));
+    let threadsig = format!(
+        "[{}][{}]",
+        no,
+        thread::current().name().unwrap_or("unknown thread")
+    );
     let (status_line, filename) = if buffer.starts_with(get) {
-        info!("{} GET /",threadsig);
+        info!("{} GET /", threadsig);
         ("HTTP/1.1 200 OK", "hello.html")
     } else if buffer.starts_with(sleep) {
-        info!("{} GET /sleep",threadsig);
+        info!("{} GET /sleep", threadsig);
         thread::sleep(Duration::from_secs(3));
         // debug!("sleep done.");
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        warn!("{} 404 NOT FOUND.req=[{}]",threadsig,str::from_utf8(&buffer).unwrap());
+        warn!(
+            "{} 404 NOT FOUND.req=[{}]",
+            threadsig,
+            str::from_utf8(&buffer).unwrap()
+        );
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
